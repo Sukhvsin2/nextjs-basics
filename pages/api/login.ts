ae9ce61c7e66f6471f0b142f7dc8913702085365
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-02-01 20:42:04 -0600

    JWT basic login with admin

---
+++ pages/api/login.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from 'jsonwebtoken'
+
+const KEY = 'askjbsoinadb;aslknas'
+
+export default function (req: NextApiRequest, res: NextApiResponse) {
+    
+    if (!req.body) return res.status(404).json({'error': 'Username & password reqired'})
+
+    const { username, password } = req.body;
+
+    res.json({
+        token: jwt.sign({
+            username,
+            admin: username === 'admin' && password === 'admin'
+        }, KEY)
+    })
+
+    res.status(200).json({message: 'OK'})
+    
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,43 @@
+import { useState } from 'react'
 import styles from '../styles/index.module.scss'
+import jwt from 'jsonwebtoken'
 
 export default function Home() {
-  return <div className={styles.container}>
-    <h1>Hello</h1>
-  </div>
+
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [loggedIn, setloggedIn] = useState<boolean>(false)
+  const [message, setMessage] = useState<string>('')
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username, password})
+    }).then((t) => t.json()).catch(e => {
+      console.log(e);
+    })
+    const token = res.token;
+
+    if (token) {
+      const json = jwt.decode(token) as { [key: string]: string }
+      setloggedIn(true)
+      setMessage(`Welcome! ${json.username}. You are ${json.admin ? 'an admin.' : 'not an admin.'}`)
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      {loggedIn ? message : <form>
+        <input placeholder='Username' autoComplete={'off'} name='username' value={username} onChange={(prop)=>setUsername(prop.target.value)} type='text' />
+        <br/>
+        <input placeholder='Password' name='password' value={password} onChange={(prop)=>setPassword(prop.target.value)} type='password' />
+        <br />
+        <button onClick={submitForm}>Login</button>
+      </form>}
+    </div>
+  )
 }
