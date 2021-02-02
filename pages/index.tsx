70540d92d0bd8176e21cb313eec4b980bbb703fd
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-02-01 19:36:55 -0600

    Global & Module css added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import { homedir } from "os";
+import styles from '../styles/index.module.css'
 
 export default function Home() {
-  return <h1>Hello</h1>
+  return <div className={styles.container}>
+    <h1>Hello</h1>
+  </div>
 }
